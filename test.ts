ab73894bba121670f8c3cbd8b43490682fd602fb
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-04-16 14:04:05 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
-// テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+input.onButtonPressed(Button.A, function () {
+    eureka_plotter_car.polygon(3, 10)
+})
+input.onButtonPressed(Button.B, function () {
+    eureka_plotter_car.polygon(5, 10)
+})
+eureka_plotter_car.microbit_version_info(microbit_version.Version2)
+eureka_plotter_car.plottercar_pen(pen_onoff.up)
+basic.forever(function () {
+	
+})
