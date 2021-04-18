b80c24eb81900e026d82c4874499c60f66352765
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-04-19 03:37:30 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -4,7 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     eureka_plotter_car.polygon(5, 10)
 })
-eureka_plotter_car.microbit_version_info(microbit_version.Version2)
+led.enable(false)
 eureka_plotter_car.plottercar_pen(pen_onoff.up)
 basic.forever(function () {
 	
